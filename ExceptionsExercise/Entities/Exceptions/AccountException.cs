042ed8c8e59023e261ac97005cbcf2df188a67df
Tiago Souza <tiagomdesouza083@gmail.com>
2025-04-07 14:56:33 -0300

    correcting folder names

---
+++ ExceptionsExercise/Entities/Exceptions/AccountException.cs
@@ -0,0 +1,9 @@
+﻿namespace Exceptions_exercise.Entities.Exceptions
+{
+    internal class AccountException : ApplicationException
+    {
+        public AccountException(string message) : base(message) 
+        {
+        }
+    }
+}
---
+++ ExceptionsExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using Exceptions_exercise.Entities;
+using System.Globalization;
+
+namespace ExceptionsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, withdrawLimit);
+
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.WithDraw(amount);
+
+                Console.WriteLine($"New balance: {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            catch (ApplicationException error)
+            {
+                Console.WriteLine($"Withdraw error: {error.Message}");
+            }
+
+        }
+    }
+}
