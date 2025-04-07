f16c50b1036005b1e26c10cc71101328032c2104
Tiago Souza <tiagomdesouza083@gmail.com>
2025-04-07 14:44:00 -0300

    adding exercise to git repository

---
+++ Exections exercise/Entities/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Exceptions_exercise.Entities.Exceptions;
+
+namespace Exceptions_exercise.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new ApplicationException("The amount exceeds withdraw limit");
+            }
+            else if (amount > Balance)
+            {
+                throw new ApplicationException("Not enough balance");
+            }
+
+            Balance -= amount;
+
+        }
+    }
+}
---
+++ Exections exercise/Entities/Exceptions/AccountException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exceptions_exercise.Entities.Exceptions
+{
+    internal class AccountException : ApplicationException
+    {
+        public AccountException(string message) : base(message) 
+        {
+        }
+    }
+}
